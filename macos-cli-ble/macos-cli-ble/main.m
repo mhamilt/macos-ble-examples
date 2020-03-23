9534bbfa69de4ad591c52d0e0a551c554d795879
Matthew <mhamilton942@gmail.com>
2020-03-23 15:07:13 +0000

    add debug macro

--- macos-cli-ble/macos-cli-ble/main.m
+++ macos-cli-ble/macos-cli-ble/main.m
@@ -4,6 +4,11 @@
 #import <Foundation/Foundation.h>
 #import <CoreBluetooth/CoreBluetooth.h>
 //------------------------------------------------------------------------------
+#define DEBUG_MODE 0
+#if DEBUG_MODE
+#pragma message "hello"
+#endif
+//------------------------------------------------------------------------------
 CBUUID *serviceUuid;
 CBUUID *characteristicUuid;
 //------------------------------------------------------------------------------
@@ -37,17 +42,19 @@ CBUUID *characteristicUuid;
 - (void)peripheralManager:(CBPeripheralManager *)peripheral central:(CBCentral *)central didUnsubscribeFromCharacteristic:(CBCharacteristic *)characteristic
 {
     _currentCentral = nil;
-    
-    printf("------------------LOST Subscription\n------------------ ");
 }
 - (void)peripheralManagerIsReadyToUpdateSubscribers:(CBPeripheralManager *)peripheral
 {
+#if DEBUG_MODE
     printf("------------------Ready To Update\n------------------ ");
+#endif
 }
 //------------------------------------------------------------------------------
 - (void)peripheralManagerDidUpdateState:(CBPeripheralManager *)peripheral
 {
+#if DEBUG_MODE
     NSLog(@"CBPeripheralManager entered state %@", [MyPeripheralManagerDelegate stringFromCBManagerState:peripheral.state]);
+#endif
     if (peripheral.state == CBManagerStatePoweredOn)
     {
         NSDictionary* dict = @{
@@ -70,7 +77,9 @@ CBUUID *characteristicUuid;
         service.characteristics = @[_mainCharacteristic];
         [self.peripheralManager addService:service];
         [self.peripheralManager startAdvertising:dict];
+#if DEBUG_MODE
         NSLog(@"startAdvertising. isAdvertising: %d", self.peripheralManager.isAdvertising);
+#endif
     }
 }
 //------------------------------------------------------------------------------
@@ -81,7 +90,9 @@ CBUUID *characteristicUuid;
     {
         NSLog(@"Error advertising: %@", [error localizedDescription]);
     }
+#if DEBUG_MODE
     NSLog(@"peripheralManagerDidStartAdvertising %d", self.peripheralManager.isAdvertising);
+#endif
 }
 
 //------------------------------------------------------------------------------
@@ -94,7 +105,9 @@ CBUUID *characteristicUuid;
         NSString* description = @"ABCD";
         request.value = [description dataUsingEncoding:NSUTF8StringEncoding];
         [self.peripheralManager respondToRequest:request withResult:CBATTErrorSuccess];
+#if DEBUG_MODE
         NSLog(@"didReceiveReadRequest:latencyCharacteristic. Responding with %@", description);
+#endif
     }
     else
     {
@@ -121,11 +134,13 @@ int main(int argc, const char * argv[])
         dispatch_queue_t ble_service = dispatch_queue_create("ble_test_service",  DISPATCH_QUEUE_SERIAL);
         CBPeripheralManager* peripheralManager = [[CBPeripheralManager alloc] initWithDelegate:peripheralManagerDelegate queue:ble_service];
         peripheralManagerDelegate.peripheralManager = peripheralManager;
+        
+        printf("waiting to send\n");
         while (![peripheralManagerDelegate currentCentral]);
         
         NSString *stringToSend = (argc > 1) ? [NSString stringWithUTF8String:argv[1]] : @"";
         [peripheralManagerDelegate sendValue:stringToSend];
-        
+        printf("waiting to sent\n");
     }
     return 0;
 }
