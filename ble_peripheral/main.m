c2502a1942bef50e9a60fc2f2ba664b6c694faaa
Matthew Hamilton <matthew.hamilton2@unibo.it>
2024-01-01 14:32:41 +0100

    fix incorrect import statement

--- ble_peripheral/main.m
+++ ble_peripheral/main.m
@@ -1,5 +1,5 @@
 #import <Foundation/Foundation.h>
-#import "MacosBlePeripheral.h"
+#import "MyPeripheralManagerDelegate.h"
 //------------------------------------------------------------------------------
 int main(int argc, const char * argv[])
 {        
